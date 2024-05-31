84555df17448b70dbe8b84447a9ea01d8bd3b2f4
Sundaram Agnihotri <agnihotrisundaram8@gmail.com>
2024-05-31 17:13:14 +0530

    redux integration

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import styles from "@/styles/auth.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const page = () => {
-  return (
-    <div>
-      Signup page
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [otp, setOtp] = useState("");
 
-export default page
+  const sendOtp = () => {};
+
+  const handleSignup = () => {};
+
+  return (
+    <div className={styles.authpage}>
+      <div className={styles.authcontainer}>
+        <h1>Signup</h1>
+        <div className={styles.inputcontaner}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="name"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.inputcontaner1}>
+          <label htmlFor="email">Email</label>
+          <div className={styles.inputrow}>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button onClick={sendOtp}>Send OTP</button>
+          </div>
+        </div>
+
+        <div className={styles.inputcontaner}>
+          <label htmlFor="otp">OTP</label>
+          <input
+            type="password"
+            name="otp"
+            id="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.inputcontaner}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button className={styles.button1} type="button" onClick={handleSignup}>
+          Signup
+        </button>
+        <Link href="/login">Already have an account?</Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/app/myfiles/page.tsx
+++ src/app/myfiles/page.tsx
@@ -1,11 +1,115 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useMemo, useState } from "react";
+import styles from "@/styles/myfiles.module.css";
+import { toast } from "react-toastify";
+import io from "socket.io-client";
 
 const page = () => {
+  const tempFiles = [
+    {
+      filename: "test1.mp4",
+      fileurl: "",
+      fileType: "video",
+      sharedAt: new Date().getDate(),
+    },
+    {
+      filename: "test2.txt",
+      fileurl: "",
+      fileType: "document",
+      sharedAt: new Date().getDate(),
+    },
+    {
+      filename: "test13.jpg",
+      fileurl: "",
+      fileType: "image",
+      sharedAt: new Date().getDate(),
+    },
+  ];
   return (
-    <div>
-      my files
-    </div>
-  )
-}
+    <div className={styles.allfiles}>
+      {tempFiles.map((file, index) => {
+        return (
+          <div key={index} className={styles.filecard}>
+            <div className={styles.left}>
+              {file.fileType == "video" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+              )}
+              {file.fileType == "image" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                  />
+                </svg>
+              )}
+              {file.fileType == "document" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+              )}
 
-export default page
+              <h3>{file.filename}</h3>
+              <p>{file.fileType}</p>
+              <p>{file.sharedAt.toString()}</p>
+            </div>
+            <div className={styles.right}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default page;
