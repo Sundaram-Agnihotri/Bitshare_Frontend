0b980d9c8b8d08354825e6ed786e234752b869e2
Sundaram Agnihotri <agnihotrisundaram8@gmail.com>
2024-06-01 16:31:15 +0530

    io integration

--- src/app/myfiles/page.tsx
+++ src/app/myfiles/page.tsx
@@ -3,8 +3,31 @@ import React, { useEffect, useMemo, useState } from "react";
 import styles from "@/styles/myfiles.module.css";
 import { toast } from "react-toastify";
 import io from "socket.io-client";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { logIn, logOut } from "@/redux/features/auth-slice";
+
+interface File {
+  createdAt: string;
+  filename: string;
+  fileurl: string;
+  fileType: string | null;
+  receiveremail: string;
+  senderemail: string;
+  sharedAt: string;
+  updatedAt: string;
+  _id: string;
+}
+
+let socket: any = null;
+let apiurl: string = `${process.env.NEXT_PUBLIC_API_URL}`;
 
 const page = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const auth = useAppSelector((state) => state.authReducer);
+  const [allFiles, setAllFiles] = useState<File[]>([]);
   const tempFiles = [
     {
       filename: "test1.mp4",
@@ -25,89 +48,161 @@ const page = () => {
       sharedAt: new Date().getDate(),
     },
   ];
+
+  const getAllFiles = async () => {
+    let res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/file/getfiles', {
+      method: 'GET',
+      credentials: 'include'
+    })
+    let resjson = await res.json();
+    if (resjson.ok) {
+      console.log(resjson.data);
+      setAllFiles(resjson.data);
+    }
+  };
+
+  const getFileType = (fileurl: any) => {
+    const extension = fileurl.split(".").pop().toLowerCase();
+
+    switch (extension) {
+      case "mp4":
+      case "avi":
+      case "mov":
+        return "video";
+
+      case "jpg":
+      case "jpeg":
+      case "png":
+      case "gif":
+        return "image";
+
+      case "pdf":
+      case "doc":
+      case "docx":
+      case "txt":
+        return "document";
+
+      default:
+        return "unknown";
+    }
+  };
+  useEffect(() => {
+    getAllFiles();
+  }, []);
+
+  const [socketId, setSocketId] = useState<string>("");
+  socket = useMemo(() => io(apiurl), []);
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(auth.isAuth);
+    if (!auth.isAuth) {
+      return router.push("/login");
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id);
+      setSocketId(socket.id);
+    });
+
+    if (auth.user) {
+      socket.emit("joinself", auth.user.email);
+    } else {
+      getuserdata().then(user => {
+        socket.emit("joinself", user.email);
+      });
+    }
+
+    // socket.on("notify", (data: any) => {
+    //   toast.info("New file shared with you" + data.from);
+    //   getAllFiles();
+    // });
+  }, []);
+
+  const getuserdata = async () => {
+    let res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/auth/getuser", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    let data = await res.json();
+    if (data.ok) {
+      dispatch(logIn(data.data));
+      return data.data;
+    } else {
+      dispatch(logOut());
+      router.push("/login");
+    }
+  };
+
   return (
     <div className={styles.allfiles}>
-      {tempFiles.map((file, index) => {
-        return (
-          <div key={index} className={styles.filecard}>
-            <div className={styles.left}>
-              {file.fileType == "video" && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
-                  />
-                </svg>
-              )}
-              {file.fileType == "image" && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                  />
-                </svg>
-              )}
-              {file.fileType == "document" && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
-                  />
-                </svg>
-              )}
+      <table>
+        <thead>
+          <tr>
+            <th>Filename</th>
+            <th>File Type</th>
+            <th>Sender Email</th>
+            <th>Receiver Email</th>
+            <th>Shared At</th>
+            <th>View</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allFiles
+            .sort((a, b) => {
+              // TODO: sort
+              return (
+                new Date(b.sharedAt).getTime() - new Date(a.sharedAt).getTime()
+              );
+            })
+            .map((file, index) => {
+              file.fileType = getFileType(file.fileurl);
 
-              <h3>{file.filename}</h3>
-              <p>{file.fileType}</p>
-              <p>{file.sharedAt.toString()}</p>
-            </div>
-            <div className={styles.right}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-            </div>
-          </div>
-        );
-      })}
+              return (
+                <tr key={index}>
+                  <td>{file.filename}</td>
+                  <td>
+                    {file.fileType == "video" && "Video"}
+                    {file.fileType == "image" && "Image"}
+                    {file.fileType == "document" && "Document"}
+                    {file.fileType == "unknown" && "Unknown"}
+                  </td>
+
+                  <td>{file.senderemail}</td>
+                  <td>{file.receiveremail}</td>
+                  <td>{new Date(file.sharedAt).toLocaleString()}</td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 };
